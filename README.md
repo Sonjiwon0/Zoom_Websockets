dc3ef7114d989c0100b00e4baffd213daa1b68a0
Jiwon <sjiwon9303@gmail.com>
2022-12-26 02:38:16 +0900

    chore: frontend setting

--- README.md
+++ README.md
@@ -5,10 +5,17 @@ Zoom Clone using NodeJS, WebRTC and Websockets.
 ## 환경 설정
 
 `git init .`
+
 `npm i nodemon -D`
+
 `npm i @babel/core -D`
+
 `npm i @babel/cli -D`
+
 `npm i @babel/node -D`
+
 `npm i express`
+
 `npm i pug`
+
 `npm i @babel/preset-env -D`
--- src/server.js
+++ src/server.js
@@ -1,6 +1,14 @@
 import express from "express";
 const app = express();
 
-console.log("hello");
+// view engine을 Pug로 설정
+app.set("view engine", "pug");
+// views 디렉토리 설정
+app.set("views", __dirname + "/views");
+// public 폴더를 유저에게 공유
+app.use("/public", express.static(__dirname + "/public"));
+// home.pug를 render 해주는 route handler 생성
+app.get("/", (req, res) => res.render("home"));
 
-app.listen(3000);
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+app.listen(3000, handleListen);
